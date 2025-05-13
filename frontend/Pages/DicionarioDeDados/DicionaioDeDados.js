a07a3e8aae94f0815ff0b1628c32ebd0f7a7c85f
Juan Marques <devjuanmarques@gmail.com>
2025-05-13 14:19:40 -0300

    02

---
+++ frontend/Pages/DicionarioDeDados/DicionaioDeDados.js
@@ -0,0 +1,28 @@
+
+fetch('dicionario_dados_plataforma_petroleo.json')
+    .then(response => response.json())
+    .then(dados => {
+        const cabecalho = document.getElementById('cabecalho');
+        const corpo = document.getElementById('corpo-tabela');
+
+        
+        if (dados.length > 0) {
+            Object.keys(dados[0]).forEach(coluna => {
+                const th = document.createElement('th');
+                th.textContent = coluna;
+                cabecalho.appendChild(th);
+            });
+        }
+
+        
+        dados.forEach(linha => {
+            const tr = document.createElement('tr');
+            Object.values(linha).forEach(valor => {
+                const td = document.createElement('td');
+                td.textContent = valor;
+                tr.appendChild(td);
+            });
+            corpo.appendChild(tr);
+        });
+    })
+    .catch(error => console.error('Erro ao carregar os dados:', error));
